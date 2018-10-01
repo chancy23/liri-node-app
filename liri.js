37c39a5a5cf7277a8133a96af17e560ec8331f31
Chancy <chancyleath@hotmail.com>
2018-10-01 16:04:00 -0600

    got spotify to pull in results for 1 track

--- liri.js
+++ liri.js
@@ -25,19 +25,46 @@ var command = process.argv[2];
 //var for the song, band, or movie title...this needs to go to the search for the API call
 var input = process.argv[3];
 
-//if movoe
+//function for spotify this commmand
+function spotifyThis() {
+    //function to search spotify with type of track and the query
+    spotify.search({type: "track", query: input, limit: 10}, function(err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+        else {
+            //*******need to do a for loop to loop thorugh the 10 results and display each line below
+            // console.log("this is all the data: " + JSON.stringify(data, null, 2)); //test
+            console.log("Artist(s): " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+            console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+            console.log("Preview: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+            console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+        }
+    });
+}
+
+//function for movie this command
 function movieThis() {
     //take input and add to omdb request URL that includes API key
     var omdbKey = keys.omdb.id;
     var omdbRequestURL = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + input;
     // send request to omdb to get the result
-    request(omdbRequestURL, function (error, body) {
+    request(omdbRequestURL, function (error, response, body) {
         if (error) {
-        return console.log("error: " + error); // Print the error if one occurred
+            // Print the error if one occurred
+            return console.log("error: " + error); 
         }
         else {
-        // console.log("statusCode: " + response && response.statusCode); // Print the response status code if a response was received
-        console.log("body: " + JSON.stringify(body, null, 2));
+            // take the body and parse out the required key data
+            // console.log("full data: " + JSON.stringify(body, null, 2)); //testing data
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Released in: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country of Production: " + JSON.parse(body).Country);
+            console.log("Languages: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
         };
     });
 };
@@ -47,17 +74,9 @@ function movieThis() {
 // totalRequest.push(command + ", " + input);
 // console.log("this is the request array: " + totalRequest);
 
-
+//actually runs the functions based on the command
 if (command === "spotify-this-song") {
-    //function to search spotify with type of track and the query
-    spotify.search({type: "track", query: input, limit: 1}, function(err, data) {
-        if (err) {
-            return console.log("Error occurred: " + err);
-        }
-        else {
-        console.log("this is all the data: " + JSON.stringify(data.tracks.items.album.name, null, 2)); 
-        }
-    });
+    spotifyThis();
 }
 else if (command === "movie-this") {
     movieThis();
